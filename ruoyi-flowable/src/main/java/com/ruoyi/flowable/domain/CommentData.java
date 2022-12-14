062843c7ff03b752d7279693e8ea0ee1f037cc8e
ningwang-arch <60875240+ningwang-arch@users.noreply.github.com>
2022-12-14 21:50:11 +0800

    init

--- ruoyi-flowable/src/main/java/com/ruoyi/flowable/domain/CommentData.java
+++ ruoyi-flowable/src/main/java/com/ruoyi/flowable/domain/CommentData.java
@@ -3,13 +3,11 @@ package com.ruoyi.flowable.domain;
 import com.alibaba.fastjson.JSON;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class CommentData implements Serializable {
     private static final long serialVersionUID = 1L;
     private Long calculatorId;
@@ -19,6 +17,15 @@ public class CommentData implements Serializable {
     private String reviewerComment;
     private String applicantComment;
 
+    public CommentData() {
+        this.calculatorId = 0L;
+        this.reviewerId = 0L;
+        this.amount = 0L;
+        this.calculatorComment = "";
+        this.reviewerComment = "";
+        this.applicantComment = "";
+    }
+
     public static CommentData fromString(String str) {
         try {
             return JSON.parseObject(str, CommentData.class);
@@ -26,7 +33,7 @@ public class CommentData implements Serializable {
             return new CommentData();
         }
     }
-    
+
 
     public String toString() {
         return JSON.toJSONString(this);
